50a7e76806c35dcfcddf27d52f70aa1a97ccf7c5
runny <watcharit_s@cmu.ac.th>
2023-07-28 06:08:49 +0700

    Finish all Q5

---
+++ src/views/passenger/PassengerAirlineDetailView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import { type AirlineItem } from '@/airline'
+import type{PropType} from 'vue'
+
+defineProps({
+    airline: {
+        type: Object as PropType<AirlineItem>,
+            require: true
+    }
+})
+</script>
+<!--  id: number
+    AirlineName: string
+    email: string
+    address: string -->
+<template>
+    <div v-if="airline">
+        <p>Id : {{ airline.id }} </p>
+        <p>  Name : {{ airline.AirlineName }}</p>
+            <p>  Email : {{ airline.email }} </p>
+                <p> Address : {{ airline.address }} </p>
+        
+    </div>
+</template>
---
+++ src/views/passenger/PassengerDetailView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import { type PassengerItem } from '@/passenger'
+import type{PropType} from 'vue'
+
+defineProps({
+    passenger: {
+        type: Object as PropType<PassengerItem>,
+            require: true
+    }
+})
+</script>
+<!-- gender: string
+    ip_address: string
+    Source: string
+    Destination: string
+    travelDate: string
+    airlineId: number -->
+<template>
+    <div v-if="passenger">
+        <p>Name :{{ passenger.first_name }}  {{ passenger.last_name }} </p> 
+       <p> Email : {{ passenger.email }} </p> 
+       <p> Gender : {{ passenger.gender }}</p> 
+       <p> Address :  {{ passenger.ip_address }}</p> 
+       <p> Source : {{ passenger.Source }} </p> 
+       <p> Destination : {{ passenger.Destination }} </p> 
+       <p> Date :  {{ passenger.travelDate }}</p> 
+       <p> AirlineID : {{ passenger.airlineId }}</p> 
+    </div>
+</template>
